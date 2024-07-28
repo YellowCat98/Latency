0d47607f5ec033134a5f223bdfd3309b6aec3d06
YellowCat98 <elcucuyisgay@gmail.com>
2024-07-28 22:52:32 +0100

    release the mod???

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,30 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/PlayerObject.hpp>
-#include <Geode/modify/CreatorLayer.hpp>
-#include <Geode/modify/MenuLayer.hpp>
+#include <Geode/modify/PlayLayer.hpp>
 
 using namespace geode::prelude;
 
+struct PlayLayerHook : Modify<PlayLayerHook, PlayLayer> {
+struct Fields {
+	int m_latency;
+	CCLabelBMFont* m_lLabel;
+};
+	bool init(GJGameLevel* level, bool useReplay, bool idk) {
+		if (!PlayLayer::init(level, useReplay, idk)) return false;
+		auto uilayer = this->m_uiLayer;
+        m_fields->m_lLabel = CCLabelBMFont::create("", "chatFont.fnt");
+        m_fields->m_lLabel->setPosition(ccp(uilayer->getContentSize().width - 30, uilayer->getContentSize().height - 5));
+        m_fields->m_lLabel->setScale(0.5f);
+        uilayer->addChild(m_fields->m_lLabel);
+		return true;
+	}
+
+
+};
+
+$register_ids(PlayLayer) {
+    auto self = reinterpret_cast<PlayLayerHook*>(this);
+}
 
 class $modify(PlayerObjectHook, PlayerObject) {
     struct Fields {
@@ -13,30 +33,31 @@ class $modify(PlayerObjectHook, PlayerObject) {
         std::chrono::time_point<std::chrono::steady_clock> eventTime;
     };
 
+
     void pushButton(PlayerButton p0) {
-        // Capture the event time when the button is pressed
         m_fields->eventTime = std::chrono::steady_clock::now();
         m_fields->m_clicked = true;
 
-        // Call the original pushButton method
         PlayerObject::pushButton(p0);
     }
 
     void releaseButton(PlayerButton p0) {
         if (m_fields->m_clicked) {
-            // Measure the latency when the button is released
             auto eventReleaseTime = std::chrono::steady_clock::now();
             m_fields->m_latency = std::chrono::duration_cast<std::chrono::milliseconds>(eventReleaseTime - m_fields->eventTime).count();
-            log::info("Latency: {}ms", m_fields->m_latency);
             m_fields->m_clicked = false;
+            
+            auto playLayer = static_cast<PlayLayerHook*>(PlayLayerHook::get());
+            if (playLayer) {
+                playLayer->m_fields->m_lLabel->setString(fmt::format("Latency: {}ms", m_fields->m_latency).c_str());
+            }
+
         }
 
-        // Call the original releaseButton method
         PlayerObject::releaseButton(p0);
     }
 
     void update(float p0) {
-        // Ensure the player updates correctly
         PlayerObject::update(p0);
     }
 };
